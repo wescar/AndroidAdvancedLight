3e648af8feeec4d1a7a331b47b8f2c26ba9f8c5b
wescar <18600554156@163.com>
2018-07-30 15:36:27 +0800

    自定义点击事件，实现GridView

--- app/src/main/java/com/wanda/advancedlight/presenter/RecyclerViewAdapter.java
+++ app/src/main/java/com/wanda/advancedlight/presenter/RecyclerViewAdapter.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 import com.wanda.advancedlight.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -22,22 +23,49 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     private List<String> mList;
     private Context mContext;
+    private OnItemClickListener mOnItemClickListener;
+    List<Integer> mHeights = new ArrayList<Integer>();
     public RecyclerViewAdapter(Context context, List<String> list){
         this.mList = list;
         this.mContext = context;
     }
 
+
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
         MyViewHolder holder = new MyViewHolder(LayoutInflater.from(mContext).inflate(R.layout.item_recycler,parent,false));
-
         return holder;
     }
 
+
+
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
+    public void onBindViewHolder(final MyViewHolder holder, final int position) {
+        for (int i=0;i<10;i++){
+            mHeights.add((int)(100+Math.random()*300));
+        }
+        ViewGroup.LayoutParams lp = holder.textView.getLayoutParams();
+        lp.height = mHeights.get(position);
+        holder.textView.setLayoutParams(lp);
         holder.textView.setText(mList.get(position));
+        if (mOnItemClickListener !=null){
+            holder.textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = holder.getLayoutPosition();
+                    mOnItemClickListener.onItemClick(holder.textView,pos);
+                }
+            });
+            holder.textView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    int pos = holder.getLayoutPosition();
+                    mOnItemClickListener.onItemLongClick(holder.textView,pos);
+                    return false;
+                }
+            });
+        }
 
     }
 
@@ -51,6 +79,14 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         notifyItemRemoved(position);
     }
 
+    public interface OnItemClickListener{
+        void onItemClick(View view,int position);
+        void onItemLongClick(View view,int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener mOnItemClickListener){
+        this.mOnItemClickListener = mOnItemClickListener;
+    }
     public class MyViewHolder extends RecyclerView.ViewHolder{
         @BindView(R.id.tv_item)
         TextView textView;
--- app/src/main/java/com/wanda/advancedlight/view/RecycleViewActivity.java
+++ app/src/main/java/com/wanda/advancedlight/view/RecycleViewActivity.java
@@ -1,10 +1,16 @@
 package com.wanda.advancedlight.view;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
+import android.widget.Toast;
 
 import com.wanda.advancedlight.BaseActivity;
 import com.wanda.advancedlight.R;
@@ -49,13 +55,39 @@ public class RecycleViewActivity extends BaseActivity {
     public void initView() {
 
         //设置布局管理器
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(4,StaggeredGridLayoutManager.VERTICAL));
         //设置item增加和删除时的动画
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
 
+//        mRecyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+
+
+
         mRecyclerViewAdapter = new RecyclerViewAdapter(this,mList);
 
+        mRecyclerViewAdapter.setOnItemClickListener(new RecyclerViewAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(RecycleViewActivity.this, "点击第"+(position+1)+"条", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemLongClick(View view, final int position) {
+
+                new AlertDialog.Builder(RecycleViewActivity.this)
+                        .setTitle("确认删除吗？")
+                        .setNegativeButton("取消",null)
+                        .setPositiveButton("正确", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mRecyclerViewAdapter.removeData(position);
+                            }
+                        }).show();
+            }
+        });
         mRecyclerView.setAdapter(mRecyclerViewAdapter);
+
 
     }
 
